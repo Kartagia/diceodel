91d64c92ad36677ef84080f4897f9206fb32a894
Antti Kautiainen <antti@kautiainen.com>
2024-07-22 21:54:16 +0300

    test: class simpledie
    modified:   test/die-model/die.simpledie.test.mjs
    - test: static method createSides
      - test case: Cteate die with non-integer sides.
      - test case: Create die with hex digits combining numbers and string.

--- test/die-model/die.simpledie.test.mjs
+++ test/die-model/die.simpledie.test.mjs
@@ -48,6 +48,40 @@ function createExpectedSides(sideCount) {
 
 describe("class SimpleDie", function () {
 
+    describe("static method createSides", function() {
+        const testCases = [
+            ["d6/2", [0.5, 3, (value) => (value+0.5)], (value) => {
+                expect(value).instanceOf(Array);
+                expect(value).length(6);
+                expect(value).eql([0.5,1,1.5,2,2.5,3]);
+            }], 
+            ["dHex", [0, "F", ( /** @type {number|string} */ value) => (typeof value === "number" ? (value < 9 ? value +1 : "A") : (value <= "F" ? String.fromCodePoint( value.codePointAt(0) + 1) : undefined)), 
+            (a,b) => (typeof a === typeof b ? a <= b : (typeof a === "number" ? -1 : 1))], 
+            (value) => {
+                console.log(`Testing ${toString(value)}`)
+                expect(value).instanceof(Array);
+                expect(value).length(16, `Invalid length for ${toString(value)}`);
+                expect(value).eql([0, 1,2,3,4,5,6, 7,8,9,"A", "B", "C", "D", "E", "F"]);
+            }]
+        ];
+        testCases.forEach( ([name, paramArray, tester, exception = undefined], index) => {
+            it(`Test #${index}: ${name}`, function() {
+                if (exception) {
+                    expect( () => {
+
+                    }).to.throw(exception);
+                } else {
+                    expect( () => {
+                        const rsult = SimpleDie.createSides(...paramArray);
+                    }).not.throw(exception);
+                    const result = SimpleDie.createSides(...paramArray);
+                    tester(result);
+                }
+
+            })
+        });
+    });
+
     const baseDice = [0, 2, 3, 4, 6, 8, 10, 12, 20, 30, 100];
 
     const testCases = [
@@ -62,7 +96,7 @@ describe("class SimpleDie", function () {
         }]))),
         ...(baseDice.map(sideCount => ([`D${sideCount} (sideCount)`, { sideCount }, (value) => {
             expect(value).eql([...createExpectedSides(sideCount)]);
-        }]))),
+        }, SyntaxError]))),
         ...(baseDice.map(sideCount => ([`D${sideCount} (object)`, { sideCount, sides: [...createExpectedSides(sideCount)] }, (value) => {
             expect(value).eql([...createExpectedSides(sideCount)]);
         }]))),
@@ -72,17 +106,22 @@ describe("class SimpleDie", function () {
         }]))),
     ];
     describe("Static method createDieSides", function () {
-        testCases.forEach(([name, param, tester], index) => {
+        testCases.forEach(([name, param, tester, exception = undefined], index) => {
             it(`Test Case #${index}: ${name}`, function () {
-                if (param === undefined || Number.isSafeInteger(param)) {
+                if (exception !== undefined) {
                     expect(function () {
                         console.log(`Creating test case ${toString(param)}`)
+                        const result = SimpleDie.createDieSides(param);
+                        tester(result);
+                    }).throw(exception);
+
+                } else if ((param === undefined || Number.isSafeInteger(param))) {
+                    expect(function () {
                         const result = SimpleDie.createDieSides(param);
                         tester(result);
                     }).not.throw();
                 } else {
                     expect(function () {
-                        console.log(`Creating test case ${toString(param)}`)
                         const result = SimpleDie.createDieSides(param);
                         tester(result);
                     }).throw(SyntaxError);
@@ -92,13 +131,21 @@ describe("class SimpleDie", function () {
         })
     });
     describe("constructor", function () {
-        testCases.forEach(([name, param, tester], index) => {
+        testCases.forEach(([name, param, tester, exception = undefined], index) => {
             it(`Test Case #${index}: ${name}`, function () {
+                if (exception) {
+                    expect(function () {
+                        console.log(`Creating test case ${toString(param)}`)
+                        const result = new SimpleDie(param);
+                        tester(result?.sides);
+                    }).throw(exception);
+                } else {
                     expect(function () {
                         console.log(`Creating test case ${toString(param)}`)
                         const result = new SimpleDie(param);
                         tester(result?.sides);
                     }).not.throw();
+                }
             });
         })
     });
