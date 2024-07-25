c672ba87b4e5b5d4e8c25f6816c6f041d1227a02
Antti Kautiainen <antti@kautiainen.com>
2024-07-26 01:06:27 +0300

    refactor: adopting letter and digit methods
    Replacing letter and digit methdos with implementations
    on the leters and digits class.
    
    modified:   test/model-reroller/combiner.test.mjs
    - refactor: replaced HexDigitComparison with leters and digits module
    version.
    - refactor: replaced checkhexDigit with letters and digits module
    version.

--- test/model-reroller/combiner.test.mjs
+++ test/model-reroller/combiner.test.mjs
@@ -2,27 +2,45 @@
 import { expect } from "chai";
 import { DefaultComparison } from "../../src/comparison.mjs";
 import { KeepBestCombiner } from "../../src/rerolling.mjs";
-import { checkInteger, checkNegativeInteger, checkPositiveInteger } from "../../src/integer.mjs";
+import { checkInteger } from "../../src/integer.mjs";
+import { checkHexDigit } from "../../src/lettersAndDigits.mjs";
+import { HexDigitComparison } from "../../src/lettersAndDigits.mjs";
 
 
-const HexDigitComparison = /** @type {Compare<number|string>}*/ (a, b) => (typeof a === typeof b ? DefaultComparison(a, b) : typeof a === "number" ? checkNegativeInteger(-1) : checkPositiveInteger(1));
-
-const testCases = [
-    {
+/**
+ * @template [PARAM=number]
+ * @type {BaseRerollPolicyTestCase<number|string, (number|string)[], SyntaxError>[]}
+ */
+const singleDieResult = [
+    /** @type {BaseRerollPolicyTestCase<Integer, Integer[], SyntaxError>} */ {
         name: "Single die (numbers)",
         construction: [checkInteger(1)],
-        params: [3, 5, 2, 1, 0]
+        params: [3, 5, 2, 1, 0].map(checkInteger)
     },
-    {
+    /** @type {BaseRerollPolicyTestCase<HexDigit, HexDigit[], SyntaxError>} */ {
         name: "Single die (hexadecimal)",
-        construction: [checkInteger(1),HexDigitComparison],
-        params: [3, 5, "F", 2, , "A", 0]
+        construction: [checkInteger(1), HexDigitComparison],
+        params: [3, 5, "F", 2, , "A", 0].map(checkHexDigit)
     }
 
 ];
 
+const multipleDieResult = [
+    /** @type {BaseRerollPolicyTestCase<Integer, Integer[], SyntaxError>} */ {
+        name: "Multiple die (numbers)",
+        construction: [checkInteger(1)],
+        params: [3, 5, 2, 1, 0].map(checkInteger)
+    },
+    /** @type {BaseRerollPolicyTestCase<HexDigit, HexDigit[], SyntaxError>} */ {
+        name: "Multiple die (hexadecimal)",
+        construction: [checkInteger(1), HexDigitComparison],
+        params: [3, 5, "F", 2, , "A", 0].map(checkHexDigit)
+    }
+
+]
+
 describe("class KeepBestCombiner", function () {
-    testCases.map((testCase) => {
+    singleDieResult.map((testCase) => {
         const params = testCase.construction;
         const compare = testCase.construction?.[1] ?? DefaultComparison;
         const expected = testCase.params.reduce((result, value) => {
